731c0c3683639870fe2e56354c671604c0e1ba7f
Peter Swanson <topazoo42@gmail.com>
2019-08-26 22:50:52 -0700

    Initial models for homepage/index

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Base_Menu_Content(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+    
+    def __repr__(self):
+        return '<{}: {}>'.format(self.__class__.__name__, self.__str__())
+
+class Category(Base_Menu_Content):
+    pass
+
+class Project(Base_Menu_Content):
+    project_category = models.ForeignKey(Category, on_delete=models.CASCADE)
