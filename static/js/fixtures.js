c7254c6f64dab6c5f97b90736bfa1e5ce75ef0ab
Peter Swanson <topazoo42@gmail.com>
2019-09-16 08:12:37 -0700

    Modular frontend that supports easily pluggable widgets

--- static/js/fixtures.js
+++ static/js/fixtures.js
@@ -1,5 +1,17 @@
-// REGISTER ANGULAR JS FIXTURES HERE:
+/**
+    FIXTURE SPECIFICS
+        modules
+            Custom Angular modules defined in any script included in the DOM should be
+            registered here. This allows any number of modules to be mixed and matched.
+        
 
+        widgets
+            Templates to compile with (optional) models from the API. Registered widgets
+            can be added to the DOM using a specified tag (e.g. <widget-name></widget-name>).
+
+        routes
+            Routes and associated pages to be used by ngRoute in Single Page Applications.
+**/
 
 var Fixtures = {
 
@@ -7,45 +19,58 @@ var Fixtures = {
 
     ],
 
-    widgets: [
+    widgets: [ 
+    /* 
+        {
+            name:               The name to use for the widget (and the html tag that renders the widget).
+            template_url:       The URL to fetch the widget template from (Optional - defaults to <<settings.default_api_url>> if 'template' not specified).
+            template:           The widget template in string form (Optional - if template_url is specified).
+            template_data_path: A dot-seperated path to use part of the API response JSON as template. 
+                                (e.g. data.templates.navbar). (Optional - defaults to <<settings.default_template_data_path>>)
+            models:             A list of objects to fetch from one or more APIs to compile the template with.
+            [
+                {
+                    model:              The name of the model to retrieve.
+                    scope_key:          The key used to store the retrieved models in the scope (and to reference them in the template) 
+                                        (Optional - defaults to the model name).
+                    url:                The url of the API to fetch the model from. (Optional - defaults to <<settings.default_api_url>>)
+                    data_path:          A dot-seperated path to use part of the API response JSON as the model 
+                                        (e.g. data.authors.Stephen_King). (Optional - defaults to <<settings.default_models_data_path>>)
+                }
+            ],
+            models_override:   A list of pre-fetched objects to compile the template with.
+            [ 
+                {
+                    model:             The name of the model (Optional). 
+                    scope_key:         The key used to store the retrieved models in the scope.
+                                       (and to reference them in the template) (Optional - defaults to 'model'
+                                       if specified otherwise <<settings.default_model_scope_key>>).
+                    
+                    ...                The rest of the pre-fetched model's attributes.
+                }
+            ],
+        }, 
+    */
         {
             name: 'header',
         },
 
         {
             name: 'sidebar',
-            models: [{'model': 'category'}]
+            models: [{'model': 'category', 'scope_key': 'categories'}],
         },
     ],
 
     routes: [
 
-    ]
+    ],
+
+    settings: {
+        default_api_url: '/api/',
+        default_models_data_path: 'data.models',
+        default_model_scope_key: 'models+',
+        default_template_data_path: 'data.models.0.template',
+        DOM_attach_point: 'body',
+    }
 };
 
-
-/**
-    FIXTURE SPECIFICS
-        modules
-            Custom Angular modules defined in any script included in the DOM should be
-            registered here. This allows any number of modules to be mixed and matched.
-        
-            Parameters:
-                name - The name of the module.
-                // TODO - services
-
-        widgets
-            Templates to compile with (optional) models from the API. Registered widgets
-            can be added to the DOM using a specified tag (e.g. <widget-name></widget-name>).
-            Templates and stylesheets used to render the widgets are delivered by the API and are stored on the 
-            server in <<STATIC_ROOT>/widgets/.
-
-            Parameters:
-                name - The name of the widget (will be the HTML tag that renders the widget).
-                models - A list of models to compile the template with, in the proper format for GET() (e.g. {'model': 'mymodel'} or 
-                        {'model': 'mymodel', 'filter': 'name:model_name'}). Optional.
-                html_template - The template to use when rendering the widget. <<STATIC_ROOT>/widgets/<<name>>.html by default. Optional.
-
-        routes
-            Routes and associated pages to be used by ngRoute in Single Page Applications.
-**/
